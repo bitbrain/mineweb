804b29ba2857ffa5c67b3c938ce2a7c10d57ad1f
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2013-11-21 22:27:41 +0100

    Added maven

---
+++ src/de/myreality/mineweb/MineWeb.java
@@ -0,0 +1,20 @@
+package de.myreality.mineweb;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MineWeb extends JavaPlugin {
+
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		super.onDisable();
+	}
+
+	@Override
+	public void onLoad() {
+		// TODO Auto-generated method stub
+		super.onLoad();
+	}
+
+	
+}
