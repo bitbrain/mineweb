2cc1716ecdb4eb4893a6ae4b5e55fcfe02e66585
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2013-11-21 22:37:25 +0100

    Added comments

--- src/de/myreality/mineweb/MineWeb.java
+++ src/de/myreality/mineweb/MineWeb.java
@@ -1,20 +1,52 @@
+/* mineweb is a Bukkit plugin for Minecraft to provide server information.
+ *  Copyright (C) 2013  Miguel Gonzalez
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ */
 package de.myreality.mineweb;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * Main plugin class which is loaded by minecraft. Handles all
+ * required submodules.
+ * 
+ * @author Miguel Gonzalez <miguel-gonzalez@gmx.de>
+ * @since 1.0
+ * @version 1.0
+ */
 public class MineWeb extends JavaPlugin {
 
-	@Override
-	public void onDisable() {
-		// TODO Auto-generated method stub
-		super.onDisable();
-	}
+	// ===========================================================
+	// Constants
+	// ===========================================================
 
-	@Override
-	public void onLoad() {
-		// TODO Auto-generated method stub
-		super.onLoad();
-	}
+	// ===========================================================
+	// Fields
+	// ===========================================================
 
-	
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+
+	// ===========================================================
+	// Methods from superclass/interface
+	// ===========================================================
+
+	// ===========================================================
+	// Other methods
+	// ===========================================================
+
+	// ===========================================================
+	// Inner classes
+	// ===========================================================
+
 }
